abe055d718d8aec98e5900a3ab3c2504b8f4b1a0
nicourn <s57111702@gmain.com>
2020-06-03 13:04:57 +0300

    Ver 0.2 Refactoring, make needed project structure

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 from screenshoter import ScreenShoter, ScreenFromTime
 from hotkeyer import KeyListener
-import os
+import os, time
 
 from_time = ScreenFromTime(ScreenShoter("time", "time/"), 3)
 from_key = KeyListener(ScreenShoter("key", "key/"))
 
 from_key.screen_shoter.get_area()
+
+img = ScreenShoter.queue.get()
+img[0].save(img[1])
 from_key.screen_shoter.fix_coord()
+to_remove = img
+os.remove(img[1])
 
 from_key.start()
-
 from_time.start()
-to_remove = []
+
+
+
+def send(img):
+    print("Sending...")
+    time.sleep(0.1)
+    print("Sended")
 
 try:
     while True:
         img = ScreenShoter.queue.get()
-        # Пересылка изображения
-        img[0].save(img[1])
-        print(f"{img[1]} saved")
 
-        if img[1].startswith("key") and len(to_remove) != 0 and img[0].tobytes() == to_remove[0].tobytes():
-            print("eq")
+        ishotkey = img[1].startswith("key")
+        notequal = img[0].tobytes() != to_remove[0].tobytes()
+
+        if ishotkey or notequal:
+            send(img)
         else:
-            print("bad")
+            print("Not send")
 
-        if len(to_remove) != 0:
-            os.remove(to_remove[1])
         to_remove = img
 
-except KeyboardInterrupt:
-    if len(to_remove) != 0:
-        os.remove(to_remove[1])
+except:
     dirs = os.listdir()
     for dir in dirs:
         if os.path.isdir(dir) and dir[0].isalpha():
-            print(dir)
             os.removedirs(dir)
